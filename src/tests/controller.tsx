13e8d8df3525e12130b5a4aff02644ae8e294ec9
Fabian Cook <hello@fabiancook.dev>
2022-04-03 23:58:23 +1200

    Show progression

--- src/tests/controller.tsx
+++ src/tests/controller.tsx
@@ -10,11 +10,15 @@ export interface Push<T> {
 
 export function createPush<T>(value: T): Push<T> {
     let resolveNext: ((value?: T) => void)[] = [];
+    let pending = false;
     let done = false;
     let object: Push<T>
     object = {
         next(next) {
             value = next;
+            if (!resolveNext.length) {
+                pending = true;
+            }
             for (const fn of resolveNext) {
                 fn(value);
             }
@@ -24,6 +28,7 @@ export function createPush<T>(value: T): Push<T> {
             return value
         },
         set value(next: T) {
+            value = next;
           object.next(next);
         },
         get done() {
@@ -32,6 +37,7 @@ export function createPush<T>(value: T): Push<T> {
         set done(next) {
             if (!next) return;
             done = next;
+            object.next(value);
         },
         name: "fragment",
         children: {
@@ -44,6 +50,10 @@ export function createPush<T>(value: T): Push<T> {
         let last = undefined;
         do {
             const promise = new Promise<T>((resolve) => resolveNext.push(resolve));
+            if (pending) {
+                object.next(value);
+                pending = false;
+            }
             await promise;
             if (done && last === value) {
                 break;
@@ -56,20 +66,33 @@ export function createPush<T>(value: T): Push<T> {
 
 function Controller() {
     const push = createPush([]);
-    return <>{push}<Run /></>
+    const name = createPush("");
+    return <>{push}<Run />Hello {name}</>
 
     // A function running alongside an async iterable allows for both to co-exist without
     // relying on one finishing or not
     async function Run() {
         push.value = [1, 2, 3];
+        name.value = "Wor";
 
         await new Promise(resolve => setTimeout(resolve, 10));
 
         push.value = [4, 5, 6];
+        name.value = "World!";
 
+        name.done = true;
         push.done = true;
     }
 }
+
+let done = false;
+
+async function d(): Promise<unknown> {
+    await new Promise<void>(r => setTimeout(r, 1));
+    if (done) return;
+    return d();
+}
+void d();
 
 const main = (
     <result>
@@ -80,3 +103,5 @@ const main = (
 for await (const string of toJSON(main)) {
     console.log(string);
 }
+
+done = true;
