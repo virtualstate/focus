6a3fa1451b6df2e204ef38fb4b8814a50a8a463e
Fabian Cook <hello@fabiancook.dev>
2022-08-28 15:05:49 +1200

    replace push implementation

--- src/tests/controller.tsx
+++ src/tests/controller.tsx
@@ -1,72 +1,9 @@
 import { h, createFragment, toJSON } from "@virtualstate/focus";
-
-export interface Push<T> {
-  next(value: T): void;
-  value: T;
-  done: boolean;
-  name: "fragment";
-  children: AsyncIterable<T>;
-}
-
-export function createPush<T>(value: T): Push<T> {
-  let resolveNext: ((value?: T) => void)[] = [];
-  let pending = false;
-  let done = false;
-  let object: Push<T>;
-  object = {
-    next(next) {
-      value = next;
-      if (!resolveNext.length) {
-        pending = true;
-      }
-      for (const fn of resolveNext) {
-        fn(value);
-      }
-      resolveNext = [];
-    },
-    get value() {
-      return value;
-    },
-    set value(next: T) {
-      value = next;
-      object.next(next);
-    },
-    get done() {
-      return done;
-    },
-    set done(next) {
-      if (!next) return;
-      done = next;
-      object.next(value);
-    },
-    name: "fragment",
-    children: {
-      [Symbol.asyncIterator]: Push,
-    },
-  };
-  return object;
-
-  async function* Push() {
-    let last = undefined;
-    do {
-      const promise = new Promise<T>((resolve) => resolveNext.push(resolve));
-      if (pending) {
-        object.next(value);
-        pending = false;
-      }
-      await promise;
-      if (done && last === value) {
-        break;
-      }
-      yield value;
-      last = value;
-    } while (!done);
-  }
-}
+import { Push } from "@virtualstate/promise";
 
 function Controller() {
-  const push = createPush([]);
-  const name = createPush("");
+  const push = new Push();
+  const name = new Push();
   return (
     <>
       {push}
@@ -78,16 +15,16 @@ function Controller() {
   // A function running alongside an async iterable allows for both to co-exist without
   // relying on one finishing or not
   async function Run() {
-    push.value = [1, 2, 3];
-    name.value = "Wor";
+    push.push([1, 2, 3]);
+    name.push("Wor");
 
     await new Promise((resolve) => setTimeout(resolve, 10));
 
-    push.value = [4, 5, 6];
-    name.value = "World!";
+    push.push([4, 5, 6]);
+    name.push("World!");
 
-    name.done = true;
-    push.done = true;
+    name.close();
+    push.close();
   }
 }
 
