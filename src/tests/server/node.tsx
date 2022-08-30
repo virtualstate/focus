cc701a4c9124e00e9a20db69dcc25b9a189e0403
Fabian Cook <hello@fabiancook.dev>
2022-08-30 22:15:41 +1200

    use writable stream in example

--- src/tests/server/node.tsx
+++ src/tests/server/node.tsx
@@ -1,47 +1,34 @@
 import { createServer } from "http";
-import { descendants, h, isString, ok} from "@virtualstate/focus";
-import {reader} from "./async-reader";
-import {Fetch} from "./fetch";
-import {memo} from "@virtualstate/memo";
-import {toJSON} from "./app";
-import {testJSXServer} from "./test-server";
+import { ok } from "@virtualstate/focus";
+import { toStream } from "./app";
+import { testJSXServer } from "./test-server";
 
 console.log("Starting server");
 const server = createServer((request, response) => {
+    void toStream().pipeTo(new WritableStream({
+        start() {
+            response.writeHead(200, {
+                "Content-Type": "application/json"
+            });
+        },
+        write(chunk) {
+            response.write(chunk);
+        },
+        close() {
+            try {
+                response.end()
+            } catch {
 
-    run()
-        .catch(error => {
-           try {
-               response.writeHead(500);
-               response.write(String(error));
-           }  catch {
+            }
+        },
+        abort() {
+            try {
+                response.end()
+            } catch {
 
-           } finally {
-               try {
-                   response.end();
-               } catch {
-               }
-           }
-
-        });
-
-
-    async function run() {
-        console.log("Request started");
-        response.writeHead(200, {
-            "Content-Type": "application/json"
-        });
-
-        for await (const part of toJSON()) {
-            response.write(part);
+            }
         }
-
-        response.end();
-        console.log("Request ended");
-    }
-
-
-
+    }));
 })
 
 await new Promise<void>(resolve => server.listen(0, resolve));
