76e65024944e6c1723521fa1f8f68c52a1902904
Fabian Cook <hello@fabiancook.dev>
2022-09-01 21:15:07 +1200

    Try close underlying source later for bun?

--- src/tests/server/app.tsx
+++ src/tests/server/app.tsx
@@ -1,4 +1,5 @@
-import { h, toJSON } from "@virtualstate/focus";
+import { h } from "@virtualstate/focus";
+import {toResponse as toResponseBase} from "./response";
 
 export async function *App() {
     console.log("Starting App");
@@ -7,58 +8,8 @@ export async function *App() {
     await new Promise(resolve => setTimeout(resolve, 500));
     console.log("Finished thing 1");
     yield <p>Loaded</p>
-    console.log("Finishing App");
 }
 
-export async function *toJSONArray(parts: AsyncIterable<string>) {
-    yield "[";
-    let first = true;
-    for await (const part of parts) {
-        if (!first) yield ",";
-        first = false;
-        yield part;
-    }
-    yield "]";
-}
-
-function toPullUnderlyingSource(iterable: AsyncIterable<string>): UnderlyingSource {
-    const encoder = new TextEncoder();
-    let iterator: AsyncIterator<string>;
-    return {
-        start() {
-          iterator = iterable[Symbol.asyncIterator]();
-        },
-        async pull(controller) {
-            const { value, done } = await iterator.next();
-            if (done) {
-                controller.close();
-            } else {
-                controller.enqueue(encoder.encode(value));
-            }
-        },
-        async cancel() {
-            await iterator.return();
-        }
-    }
-}
-
-export function toStream(node: unknown) {
-    const source = toPullUnderlyingSource(
-        toJSONArray(
-            toJSON(node)
-        )
-    );
-    return new ReadableStream(source);
-}
-
-export function toResponse(node: unknown = <App />) {
-    return new Response(
-        toStream(node),
-        {
-            headers: {
-                "Content-Type": "application/json",
-                "X-Content-Type-Options": "nosniff"
-            }
-        }
-    )
+export function toResponse() {
+    return toResponseBase(<App />);
 }
---
+++ src/tests/server/response.ts
@@ -0,0 +1,61 @@
+import {toJSON} from "@virtualstate/focus";
+
+export async function *toJSONArray(parts: AsyncIterable<string>) {
+    yield "[";
+    let first = true;
+    for await (const part of parts) {
+        if (!first) yield ",";
+        first = false;
+        yield part;
+    }
+    yield "]";
+}
+
+declare var Bun: unknown;
+
+function toPullUnderlyingSource(iterable: AsyncIterable<string>): UnderlyingSource {
+    const encoder = new TextEncoder();
+    let iterator: AsyncIterator<string>;
+    return {
+        start() {
+            iterator = iterable[Symbol.asyncIterator]();
+        },
+        async pull(controller) {
+            const { value, done } = await iterator.next();
+            if (done) {
+                if (typeof Bun === "undefined") {
+                    controller.close();
+                } else {
+                    await new Promise(resolve => setTimeout(resolve, 500));
+                    controller.close();
+                }
+            } else {
+                controller.enqueue(encoder.encode(value));
+            }
+        },
+        async cancel() {
+            await iterator.return();
+        }
+    }
+}
+
+export function toStream(node: unknown) {
+    const source = toPullUnderlyingSource(
+        toJSONArray(
+            toJSON(node)
+        )
+    );
+    return new ReadableStream(source);
+}
+
+export function toResponse(node: unknown) {
+    return new Response(
+        toStream(node),
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "X-Content-Type-Options": "nosniff"
+            }
+        }
+    )
+}
